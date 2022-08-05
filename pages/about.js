86874ac6e26e25c2e89169e8136f7e829644ee99
faze20 <42577269+faze20@users.noreply.github.com>
2022-08-05 13:47:57 -0700

    abt1

--- pages/about.js
+++ pages/about.js
@@ -1,36 +1,50 @@
 import Image from 'next/image'
-import Link from 'next/link'
 
 function About() {
   return (
-    <div className='px-2'>
-      <div >
-        <Image
-          src='/about.jpg'    
-          alt="logo" 
-          width={100}
-          height={500}
-          layout="responsive"
-          objectFit="contain"
-          quality={100}
-          priority
-          />
-
+    <div>
+      <div className=' h-52 justify-center '>
+          <div  className='relative w-full h-full border border-gray-200 rounded-md overflow-hidden bg-black' >
+            <Image
+              src='/about.jpg'    
+              alt="logo" 
+              layout="fill"
+              objectFit="cover"
+              quality={100}
+              priority
+              className='bg-blend-overlay bg-black opacity-60'
+              />
+            <h2 className='absolute top-20 left-0 right-0 text-center text-white font-bold lg:text-2xl '>About Softwaredev Bytes Academy</h2>
+          </div>
       </div>
-        <h2 className='font-bold text-2xl mt-4'>About Softwaredev Bytes Academy</h2>
-            {/* <div className="flex items-center justify-center mt-8 mb-12">
-                <Link href='/'> 
-                    <a className='mt-2'>
-                        <Image
-                        src='/underconstruction.webp'    
-                        alt="logo" 
-                        width={1080}
-                        height={500}
-                        className='rounded-md'
-                        />
-                    </a>
-                </Link>
-            </div> */}
+      <div>
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iure perferendis
+             non adipisci porro vitae, repudiandae fugit in labore debitis vel eveniet provident 
+             molestiae rerum ducimus illo tenetur natus sit!
+          </p>
+        </div>
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iure perferendis
+             non adipisci porro vitae, repudiandae fugit in labore debitis vel eveniet provident 
+             molestiae rerum ducimus illo tenetur natus sit!
+          </p>
+        </div>
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iure perferendis
+             non adipisci porro vitae, repudiandae fugit in labore debitis vel eveniet provident 
+             molestiae rerum ducimus illo tenetur natus sit!
+          </p>
+        </div>
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iure perferendis
+             non adipisci porro vitae, repudiandae fugit in labore debitis vel eveniet provident 
+             molestiae rerum ducimus illo tenetur natus sit!
+          </p>
+        </div>
+      </div>
+
+
     </div>
   )
 }
